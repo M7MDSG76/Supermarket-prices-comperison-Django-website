e5c0b4396206574e7866a71911e7d201cfc10c13
M7MDSG76 <mohammed.s.alghanmi@gmail.com>
2022-06-19 15:25:14 +0300

    admin update: add catagory to item display list. Remove branch from filter list, and add catagory and supermarket

--- Products_prices/admin.py
+++ Products_prices/admin.py
@@ -24,10 +24,28 @@ class AdminProduct(admin.ModelAdmin):
 class AdminSupermarketBranch(admin.ModelAdmin):
     fields = ['supermarket','city','neighborhood']
     list_filter = ['supermarket', 'city', 'neighborhood']
- 
+    
+    
+
+    
 @admin.register(Item)   
 class AdminItem(admin.ModelAdmin):
-    fields = ['product', 'supermarket_branch','creator','price']
-    list_display = ('product', 'price', 'supermarket_branch', 'creator', )
-    list_filter = ['price', 'supermarket_branch', 'creator', 'product', 'register_date']
+    # Fileds in the item view
+    fields = ['product', 'supermarket_branch','creator','price',]
+    
+    # list of attribute displayed in the list item view
+    list_display = ('product', 'price', 'get_brach_supermarket', 'creator','get_product_catagory' )
+    
+    # Filter attribute in the list item view 
+    list_filter = ['price', 'supermarket_branch__supermarket', 'creator', 'product', 'register_date', 'product__catagory_name']
 
+
+    # get ManyTOMany field to the admin view 
+    @admin.display(description= 'Catagory', ordering= 'product__catagory_name', empty_value='N/A')
+    def get_product_catagory(self, obj):
+        return obj.product.catagory_name
+    
+    # get ManyTOMany field to the admin view
+    @admin.display(description= 'Supermarket', ordering= 'supermarket_branch__supermarket', empty_value='N/A')
+    def get_brach_supermarket(self, obj):
+        return obj.supermarket_branch.supermarket
