ae9d5807631d9be3c1a785d6923622047e08e00b
M7MDSG76 <mohammed.s.alghanmi@gmail.com>
2022-06-28 17:04:50 +0300

    add items list view

--- Products_prices/urls.py
+++ Products_prices/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
-from Products_prices import views
-urlpattrens = [
-    path('comparison/', views.comparisonView, name='compare-main' ),
+from django.urls import path, include
+from . import views
+
+
+print('----------------------------------------test urls.py----------------------------------------')
+urlpatterns = [
+    path('comparison/', views.ItemListView.as_view(), name='home' ),
     # path(),
 ]
--- Products_prices/views.py
+++ Products_prices/views.py
@@ -1,3 +1,24 @@
+from os import name
 from django.shortcuts import render
+from .models import *
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class ItemListView(ListView):
+    model = Item
+    template_name = 'items\items_list.html'
+    context_object_name = 'items'
+    queryset: Item.objects.all().order_by('supermarket_branch__supermarket')
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ItemListView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+
+        context.update({
+        'supermarkets' : Supermarket.objects.all(),
+        'products': Product.objects.all().order_by('name')
+    })
+        return context
+        
+
+
