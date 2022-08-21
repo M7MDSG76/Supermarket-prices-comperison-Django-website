0a64d13dde76fa9725f53f5c8473cb9fa7ada41b
M7MDSG76 <mohammed.s.alghanmi@gmail.com>
2022-08-21 15:46:30 +0300

    new migrations

--- Products_prices/models.py
+++ Products_prices/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
 
-from phonenumber_field.modelfields import PhoneNumberField
+
 
 
 
